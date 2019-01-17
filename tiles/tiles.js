d35b088c27a8d78087e1a7dc80847e738f029455
Joaquim Neto <joaquimmy@yahoo.com>
2019-01-17 00:00:23 -0200

    Cleaned the code, add a collectible, remade the gameData object

--- tiles/tiles.js
+++ tiles/tiles.js
@@ -10,9 +10,28 @@ for (let y = 0; y < 20; y++) {
   }
 }
 
+HELP = {
+  query: (x, y) => `[x="${x}"][y="${y}"]`,
+  qS: q => document.querySelector(q),
+  qSA: q => document.querySelectorAll(q)
+};
+
 /* Game Data */
-const gameData = {
-  position: {}
+const GAME = {
+  mapSize: 20,
+  player: { x: 0, y: 0 },
+  item: { x: 0, y: 0 },
+  POSITION: {
+    randomX: () => Math.floor(Math.random() * GAME.mapSize),
+    randomY: () => Math.floor(Math.random() * GAME.mapSize),
+    randomXY: function() {
+      return { x: this.randomX(), y: this.randomY() };
+    },
+    getDomElementAt: (x, y) => document.querySelector(HELP.query(x, y)),
+    getPlayerElement: () => HELP.qS(HELP.query(GAME.player.x, GAME.player.y)),
+    getItemElement: () => HELP.qS(HELP.query(GAME.item.x, GAME.item.y)),
+    isSame: (pos1, pos2) => pos1.x === pos2.x && pos1.y === pos2.y
+  }
 };
 
 /**
@@ -20,14 +39,14 @@ const gameData = {
  * @param {Int} x X offset
  * @param {Int} y Y offset
  */
-const renderPlayer = (x, y) => {
+const movePlayer = (x, y) => {
   // current player position
-  pX = gameData.position.x;
-  pY = gameData.position.y;
+  pX = GAME.player.x;
+  pY = GAME.player.y;
 
   // get current and next squares
-  const oldPos = document.querySelector(`[x="${pX}"][y="${pY}"]`);
-  const newPos = document.querySelector(`[x="${pX + x}"][y="${pY + y}"]`);
+  const oldPos = GAME.POSITION.getDomElementAt(pX, pY);
+  const newPos = GAME.POSITION.getDomElementAt(pX + x, pY + y);
 
   // update the dom
   oldPos.removeAttribute("style");
@@ -35,37 +54,37 @@ const renderPlayer = (x, y) => {
   newPos.style.zIndex = 100;
 
   // save new position
-  gameData.position = { x: pX + x, y: pY + y };
+  GAME.player = { x: pX + x, y: pY + y };
+
+  if (GAME.POSITION.isSame(GAME.player, GAME.item)) {
+    GAME.item = { ...GAME.POSITION.randomXY() };
+    GAME.POSITION.getItemElement().style.backgroundColor = "goldenrod";
+  }
 };
 
-// Start game
+// Start game -------------------------------------------------
 (() => {
-  // random start position
-  const pos = {
-    x: Math.floor(Math.random() * 20),
-    y: Math.floor(Math.random() * 20)
-  };
+  // save starter position
+  GAME.player = { ...GAME.POSITION.randomXY() };
 
   // place player there
-  const init = document.querySelector(`[x="${pos.x}"][y="${pos.y}"]`);
-  init.style.backgroundColor = "dodgerblue";
-  init.style.zIndex = 100;
+  const initial = GAME.POSITION.getPlayerElement();
+  initial.style.backgroundColor = "dodgerblue";
+  initial.style.zIndex = 100;
 
-  // save starter position
-  gameData.position = { ...pos };
+  GAME.item = { ...GAME.POSITION.randomXY() };
+  GAME.POSITION.getItemElement().style.backgroundColor = "goldenrod";
 
   // input handler
   document.body.addEventListener("keydown", e => {
-    const k = e.code;
-    const x = gameData.position.x;
-    const y = gameData.position.y;
-    if ((k === "KeyW" || k === "ArrowUp") && y !== 0)
-      renderPlayer(0, -1);
-    if ((k === "KeyD" || k === "ArrowRight") && x !== 19)
-      renderPlayer(1, 0);
-    if ((k === "KeyS" || k === "ArrowDown") && y !== 19)
-      renderPlayer(0, 1);
-    if ((k === "KeyA" || k === "ArrowLeft") && x !== 0)
-      renderPlayer(-1, 0);
+    const key = e.code;
+    const x = GAME.player.x;
+    const y = GAME.player.y;
+    if ((key === "KeyW" || key === "ArrowUp") && y !== 0) movePlayer(0, -1);
+    if ((key === "KeyD" || key === "ArrowRight") && x !== GAME.mapSize - 1)
+      movePlayer(1, 0);
+    if ((key === "KeyS" || key === "ArrowDown") && y !== GAME.mapSize - 1)
+      movePlayer(0, 1);
+    if ((key === "KeyA" || key === "ArrowLeft") && x !== 0) movePlayer(-1, 0);
   });
 })();
